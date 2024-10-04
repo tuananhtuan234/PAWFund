8d7358414724b4dbd7458ae5cd9af91742668cea
Trương Anh <tuananh123tuankiet@gmail.com>
2024-10-04 11:12:02 +0700

    tuananh fix User Repository

--- Services/Models/AppSetting.cs
+++ Services/Models/AppSetting.cs
@@ -9,5 +9,7 @@ namespace Repository.Models
     public class AppSetting
     {
         public string SecretKey { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
     }
 }
