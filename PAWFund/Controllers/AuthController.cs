0c73ccb66ed891e9507b149451975ff65182b289
phamlenhathoang <Nhathoang1470@gmail.com>
2024-10-04 11:13:16 +0700

    nhathoang fix authen

---
+++ PAWFund/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interface;
+using System.ComponentModel.DataAnnotations;
+
+namespace PAWFund.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authenService;
+
+        public AuthController(IAuthService authenService)
+        {
+            _authenService = authenService;
+        }
+
+        [HttpGet("Login")]
+        public async Task<IActionResult> Login([FromQuery][Required] string username, [FromQuery][Required] string password)
+        {
+            try
+            {
+                var result = await _authenService.Login(username, password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //[HttpPost("Register")]
+        //public Task<IActionResult> Register([FromBody][])
+    }
+}
---
+++ Services/Interface/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interface
+{
+    public interface IAuthService
+    {
+        Task<ServiceResponse<string>> Login(string username, string password);
+    }
+}
