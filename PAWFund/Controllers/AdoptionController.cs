5572709e18e0c1350474de520b3283bc64590d59
phamlenhathoang <Nhathoang1470@gmail.com>
2024-10-06 11:48:01 +0700

    Get adoption by adopion id and get all

--- PAWFund/Controllers/AdoptionController.cs
+++ PAWFund/Controllers/AdoptionController.cs
@@ -16,6 +16,36 @@ namespace PAWFund.Controllers
             _adoptionService = adoptionService;
         }
 
-        
+        [HttpGet("adoptions")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var result = await _adoptionService.GetAllAdoption(null);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet("adoption")]
+        public async Task<IActionResult> GetAdoptionByAdoptionId([FromQuery][Required] string adoptionId)
+        {
+            try
+            {
+                var result = await _adoptionService.GetAllAdoption(adoptionId);
+                if (result.Count == 0)
+                {
+                    return BadRequest("Not found");
+                }
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
